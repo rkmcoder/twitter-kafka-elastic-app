4ce089a137b6add1bdf88cf5f5daa3abb245065c
rkmcoder <rkm.bitmesra@gmail.com>
2020-05-25 02:02:39 +0530

    Added Kafka Producer

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -1,5 +1,9 @@
 package constants;
 
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
 public class Constants {
 
 	//Twitter api
@@ -7,5 +11,12 @@ public class Constants {
 	public static final String API_SECRET_KEY = "";
 	public static final String ACCESS_TOKEN = "";
 	public static final String ACCESS_TOKEN_SECRET = "";
+
+	//Search terms for tweets
+	public static final List<String> terms = Lists.newArrayList("coronavirus", "covid-19");
+
+	//Kafka config
+	public static final String BOOTSTRAP_SERVER = "localhost:9092";
+	public static final String TOPIC = "twitter_tweets";
 
 }
--- src/main/java/kafka/TwitterProducer.java
+++ src/main/java/kafka/TwitterProducer.java
@@ -1,6 +1,5 @@
 package kafka;
 
-import com.google.common.collect.Lists;
 import com.twitter.hbc.ClientBuilder;
 import com.twitter.hbc.core.Client;
 import com.twitter.hbc.core.Constants;
@@ -10,10 +9,12 @@ import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
@@ -35,37 +36,66 @@ public class TwitterProducer {
 		log.info("twitter-kafka-elastic-app stated....");
 
 		BlockingQueue<String> msgQueue = new LinkedBlockingQueue(1000);
-		Client client = createTwitterClient(msgQueue);
+		final Client client = createTwitterClient(msgQueue);
 
 		log.info("Attempting to connect twitter api....");
 		client.connect();
 		log.info("Connection established!!!!");
 
-		int x = 0;
+		final KafkaProducer<String, String> producer = createProducer();
 
-		while(!client.isDone() && x < 5) {
+		// shutdown hook to stop the client and close producer
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+				log.info("Shutting down application....");
+				client.stop();
+				producer.close();
+				log.info("Done!!!!");
+		}));
+
+		while(!client.isDone()) {
 			String tweet = null;
-
 			try {
 				tweet = msgQueue.poll(5, TimeUnit.SECONDS);
-				x++;
 			} catch (InterruptedException e) {
 				client.stop();
-				e.printStackTrace();
+				log.error("Error Occurred", e);
 			}
 
 			if (tweet != null) {
-				System.out.println(tweet);
+				log.info(tweet);
+				producer.send(new ProducerRecord<>(TOPIC, null, tweet), new Callback() {
+					public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+						if(e != null){
+							log.error("Error Occurred", e);
+						}
+					}
+				});
 			}
 		}
 
+	}
+
+	private KafkaProducer<String, String> createProducer() {
+		Properties properties = new Properties();
+		// create basic properties
+		properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+		properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+		// create safe Producer
+		properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+		properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");//idempotence default
+		properties.setProperty(ProducerConfig.RETRIES_CONFIG, String.valueOf(Integer.MAX_VALUE));//idempotence default
+		properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");//idempotence default - controls how many producer requests can be made a parallel to a single broker.
+
+		KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+		return producer;
 	}
 
 	public Client createTwitterClient(BlockingQueue<String> msgQueue) {
 
 		Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
 		StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
-		List<String> terms = Lists.newArrayList("coronavirus", "covid-19");
 		hosebirdEndpoint.trackTerms(terms);
 		Authentication hosebirdAuth = new OAuth1(API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
 
