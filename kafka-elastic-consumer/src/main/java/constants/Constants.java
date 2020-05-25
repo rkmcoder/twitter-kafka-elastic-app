be7ccc5426161cca74b2b8df46cdf28dbf7a823f
rkmcoder <rkm.bitmesra@gmail.com>
2020-05-25 18:10:17 +0530

    Elastic Client added

--- kafka-elastic-consumer/src/main/java/constants/Constants.java
+++ kafka-elastic-consumer/src/main/java/constants/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
 	//ElasticSearch config
 	public static final String ELASTICSEARCH_SERVER = "localhost";
-	public static final Integer PORT = 9092;
+	public static final Integer PORT = 9200;
 	public static final String SCHEME = "http";
 	public static final String INDEX = "twitter";
 
--- kafka-elastic-consumer/src/main/java/elasticsearch/TwitterElasticConsumer.java
+++ kafka-elastic-consumer/src/main/java/elasticsearch/TwitterElasticConsumer.java
@@ -1,5 +1,42 @@
 package elasticsearch;
 
+import constants.Constants;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+import static constants.Constants.*;
+
 public class TwitterElasticConsumer {
 
+	final static Logger log = LoggerFactory.getLogger(TwitterElasticConsumer.class);
+
+	public static void main(String[] args) throws IOException {
+
+		RestHighLevelClient restHighLevelClient = createElasticsearchClient();
+		String json = "{\"foo\" : \"bar\"}";
+		IndexRequest indexRequest = new IndexRequest(Constants.INDEX).source(json, XContentType.JSON);
+
+		log.info(indexRequest.toString());
+		IndexResponse indexResponse = restHighLevelClient.index(indexRequest, RequestOptions.DEFAULT);
+		log.info(indexResponse.getId());
+
+	}
+
+	private static RestHighLevelClient createElasticsearchClient() {
+
+		RestHighLevelClient client = new RestHighLevelClient(
+				RestClient.builder(
+						new HttpHost(ELASTICSEARCH_SERVER, PORT, SCHEME)));
+
+		return client;
+	}
 }
